6f2899cebd28c330274da735f814bd3e9770113a
Monir Ullah <mullah725@gmail.com>
2024-02-21 20:46:48 +0600

    comments

---
+++ 1/main.js
@@ -0,0 +1,20 @@
+const p = document.querySelector("#dragID");
+const div = document.querySelector(".dragDiv");
+
+// fire when drag start
+p.addEventListener("dragstart", function (e) {
+  e.dataTransfer.setData("text", e.target.id);
+});
+
+// fire when drag over in the div
+div.addEventListener("dragover", function (e) {
+  e.preventDefault();
+});
+
+// fire when drop element
+div.addEventListener("drop", function (e) {
+  const id = e.dataTransfer.getData("text");
+  const textData = document.getElementById(id);
+  div.appendChild(textData);
+  e.preventDefault();
+});
